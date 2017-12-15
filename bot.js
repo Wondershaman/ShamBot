f220558918a2b751af69e2609286535de856e0e6
Wondershaman <34525620+Wondershaman@users.noreply.github.com>
2017-12-15 03:01:40 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === 'Super') {
+    if (message.content === 'Super'|'super') {
     	message.channel.send('Sonic!');
   	}
 });
